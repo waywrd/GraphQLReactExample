d77fefa53d7d14d5fc42cc6ee8677f72a7212436
Hristiyan Dimitrov <hdimitrov1511@gmail.com>
2019-12-01 20:20:35 +0200

    Add queries

---
+++ my-app/src/queries/queries.js
@@ -0,0 +1,49 @@
+import { gql } from "apollo-boost";
+
+const getAuthors = gql`
+  {
+    authors {
+      name
+      id
+    }
+  }
+`;
+
+const getBooks = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
+
+const addBookMutation = gql`
+  mutation($name: String!, $genre: String!, $authorId: ID!) {
+    addBook(name: $name, genre: $genre, authorId: $authorId) {
+      name
+      id
+    }
+  }
+`;
+
+const getBook = gql`
+  query($id: ID) {
+    book(id: $id) {
+      id
+      name
+      genre
+      author {
+        id
+        name
+        age
+        books {
+          name
+          id
+        }
+      }
+    }
+  }
+`;
+
+export { getAuthors, getBooks, addBookMutation, getBook };
